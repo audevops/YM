2d2c52ec2900591ea106f24b870314fbca7d093f
Yash Mourya <yashmourya29@gmail.com>
2023-07-15 06:59:27 +1000

    YM Storage

---
+++ Azure_storage/variable.tf
@@ -0,0 +1,30 @@
+variable "subscription_id" {
+  type        = string
+  description = "Enter here the subscription id"
+  default     = "0cfe2870-d256-4119-b0a3-16293ac11bdc"
+}
+variable "tenant_id" {
+  type        = string
+  description = "Enter here the Tenant id"
+  default     = "84f1e4ea-8554-43e1-8709-f0b8589ea118"
+}
+variable "client_secret" {
+  type        = string
+  description = "Enter here the subscription id"
+  default     = "2XZ8Q~-wLmX5EnnsjVqjnluybICFY_gl6EkfNcFn"
+}
+variable "client_id" {
+  type        = string
+  description = "Enter here the Client id"
+  default     = "406165d9-3c89-4525-a905-b2d3c70a4f9c"
+}
+variable "resource_group_name" {
+  type        = string
+  description = "Enter here the Resource Group"
+  default     = "1-8ac1f286-playground-sandbox"
+}
+variable "location" {
+  type        = string
+  description = "Enter here the location"
+  default     = "East US"
+}
