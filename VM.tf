e3abd3b795dc83e8e30b7540f142a9a238ae9634
Yash Mourya <yashmourya29@gmail.com>
2023-07-21 06:36:55 +1000

    VM ubuntu and appache

--- VM.tf
+++ VM.tf
@@ -1,80 +1,71 @@
-provider "azurerm" {
-  features {}
-}
+  /*resource "azurerm_virtual_network" "DB_Vnet" {
+    name                = "DB__Vnet"
+    address_space       = ["10.0.0.0/16"]
+    location            = var.location
+    resource_group_name = var.resource_group_name
+  }*/
 
-resource "azurerm_resource_group" "example" {
-  name     = "my-resource-group"
-  location = "East US"
-}
-
-resource "azurerm_virtual_network" "example" {
-  name                = "my-virtual-network"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-}
-
-resource "azurerm_subnet" "example" {
-  name                 = "my-subnet"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
-  address_prefix       = "10.0.1.0/24"
-}
-
-resource "azurerm_public_ip" "example" {
-  name                = "my-public-ip"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  allocation_method   = "Static"
-}
-
-resource "azurerm_network_interface" "example" {
-  name                = "my-nic"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-
-  ip_configuration {
-    name                          = "my-nic-cfg"
-    subnet_id                     = azurerm_subnet.example.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.example.id
-  }
-}
-
-resource "azurerm_virtual_machine" "example" {
-  name                  = "my-vm"
-  location              = azurerm_resource_group.example.location
-  resource_group_name   = azurerm_resource_group.example.name
-  network_interface_ids = [azurerm_network_interface.example.id]
-  vm_size               = "Standard_DS1_v2"
-
-  storage_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
-    version   = "latest"
+  resource "azurerm_subnet" "DB_sub" {
+    name                 = "DB-subnet"
+    resource_group_name  = var.resource_group_name
+    virtual_network_name = "DB_Vnet"
+    address_prefixes     = ["10.0.0.0/24"]
+    /*depends_on = [
+      azurerm_virtual_network.DB_Vnet
+    ]*/
   }
 
-  storage_os_disk {
-    name              = "my-os-disk"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
+  resource "azurerm_public_ip" "MON_pub_IP_4" {
+    name                = "MON-public-ip-4"
+    location            = var.location
+    resource_group_name = var.resource_group_name
+    allocation_method   = "Dynamic"
   }
 
-  os_profile {
-    computer_name  = "myvm"
-    admin_username = "myadmin"
-    custom_data    = base64encode(<<EOF
-#!/bin/bash
-sudo apt-get update
-sudo apt-get install -y apache2
-sudo service apache2 start
-EOF
-    )
+
+  resource "azurerm_network_interface" "MON-nic" {
+    name                = "MON-nic"
+    location            = var.location
+    resource_group_name = var.resource_group_name
+    ip_configuration {
+      name                          = "MON-nic-configuration"
+      subnet_id                     = azurerm_subnet.DB_sub.id
+      private_ip_address_allocation = "Dynamic"
+      public_ip_address_id          = azurerm_public_ip.MON_pub_IP_4.id
+    }
   }
 
-  tags = {
-    environment = "production"
+  resource "azurerm_linux_virtual_machine" "MON1" {
+    name                = "MON1"
+    location            = var.location
+    resource_group_name = var.resource_group_name
+    size                = "Standard_D2s_v3"
+    admin_username      = "adminuser"
+    network_interface_ids = [
+      azurerm_network_interface.MON-nic.id,
+    ]
+    admin_password                  = "Proximus#18"
+    disable_password_authentication = false
+
+    os_disk {
+      caching              = "ReadWrite"
+      storage_account_type = "Standard_LRS"
+    }
+
+    source_image_reference {
+      publisher = "Canonical"
+      offer     = "0001-com-ubuntu-server-focal"
+      sku       = "20_04-lts"
+      version   = "latest"
+    }
+
+    provisioner "remote-exec" {
+      inline = [
+        "sudo apt-get update",
+        "sudo apt-get install -y apache2",
+        "sudo ufw allow 'Apache'",
+        "sudo systemctl enable apache2",
+        "sudo systemctl start apache2"
+      ]
+    }
   }
-}
