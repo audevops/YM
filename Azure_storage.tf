bb732b9b533d2a8b4b7e17320aa80d158c56985d
Yash Mourya <yashmourya29@gmail.com>
2023-07-21 06:21:46 +1000

    VM ubuntu and appache

--- Azure_storage.tf
+++ Azure_storage.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_account" "example" {
-  name                     = "yashmdev"
+  name                     = "yaashmdev"
   resource_group_name      = var.resource_group_name
   location                 = var.location
   account_tier             = "Standard"
