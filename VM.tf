c3436653ff835c84c47a8f4e8f81de4969f0a525
Yash Mourya <yashmourya29@gmail.com>
2023-07-21 05:46:17 +1000

    VM ubuntu and appache

--- VM.tf
+++ VM.tf
@@ -1,75 +1,71 @@
-provider "azurerm" {
-  features {}
-}
-
-resource "azurerm_resource_group" "myresourcegroup" {
-  name     = "my-resource-group"
-  location = "East US"
-}
-
-resource "azurerm_virtual_network" "mynetwork" {
-  name                = "my-network"
+/*resource "azurerm_virtual_network" "DB_Vnet" {
+  name                = "DB_Vnet"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.myresourcegroup.location
-  resource_group_name = azurerm_resource_group.myresourcegroup.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+}*/
+
+resource "azurerm_subnet" "DB_sub" {
+  name                 = "DB-subnet"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = "DB_Vnet"
+  address_prefixes     = ["10.0.0.0/24"]
+  /*depends_on = [
+     azurerm_virtual_network.DB_Vnet
+  ]*/
 }
 
-resource "azurerm_subnet" "mysubnet" {
-  name                 = "my-subnet"
-  resource_group_name  = azurerm_resource_group.myresourcegroup.name
-  virtual_network_name = azurerm_virtual_network.mynetwork.name
-  address_prefixes     = ["10.0.1.0/24"]
+resource "azurerm_public_ip" "MON_pub_IP_4" {
+  name                = "MON-public-ip-4"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  allocation_method   = "Dynamic"
 }
 
-resource "azurerm_public_ip" "mypublicip" {
-  name                = "my-public-ip"
-  location            = azurerm_resource_group.myresourcegroup.location
-  resource_group_name = azurerm_resource_group.myresourcegroup.name
-  allocation_method   = "Static"
-}
 
-resource "azurerm_network_interface" "mynic" {
-  name                = "my-network-interface"
-  location            = azurerm_resource_group.myresourcegroup.location
-  resource_group_name = azurerm_resource_group.myresourcegroup.name
-
+resource "azurerm_network_interface" "MON-nic" {
+  name                = "MON-nic"
+  location            = var.location
+  resource_group_name = var.resource_group_name
   ip_configuration {
-    name                          = "my-ip-configuration"
-    subnet_id                     = azurerm_subnet.mysubnet.id
+    name                          = "MON-nic-configuration"
+    subnet_id                     = azurerm_subnet.DB_sub.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.mypublicip.id
+    public_ip_address_id          = azurerm_public_ip.MON_pub_IP_4.id
   }
 }
 
-resource "azurerm_linux_virtual_machine" "myvm" {
-  name                = "my-vm"
-  location            = azurerm_resource_group.myresourcegroup.location
-  resource_group_name = azurerm_resource_group.myresourcegroup.name
-  size                = "Standard_B1s"
-  admin_username      = "myadmin"
+
+resource "azurerm_linux_virtual_machine" "MON1" {
+  name                = "MON1"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  size                = "Standard_D2s_v3"
+  admin_username      = "adminuser"
   network_interface_ids = [
-    azurerm_network_interface.mynic.id,
+    azurerm_network_interface.MON-nic.id,
   ]
+  admin_password                  = "Proximus#18"
+  disable_password_authentication = false
+
   os_disk {
-    name              = "my-os-disk"
-    caching           = "ReadWrite"
+    caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
+
   source_image_reference {
     publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "20.04-LTS"
+    offer     = "0001-com-ubuntu-server-focal"
+    sku       = "20_04-lts"
     version   = "latest"
   }
-
-  custom_data = <<-EOF
-              #!/bin/bash
-              apt-get -y update
-              apt-get -y install apache2
-              EOF
 }
-
-output "public_ip_address" {
-  value = azurerm_public_ip.mypublicip.ip_address
-}
-
+provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get update",
+      "sudo apt-get install -y apache2",
+      "sudo ufw allow 'Apache'",
+      "sudo systemctl enable apache2",
+      "sudo systemctl start apache2"
+    ]
+  }
